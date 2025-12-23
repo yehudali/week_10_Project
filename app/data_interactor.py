6130ab51c4f80ab90c11d900402e97579f102f29
Yehuda Linker! <uda.linker@gmail.com>
2025-12-23 16:19:48 +0200

    add : func "get_all_contacts->list[Contact]:

--- app/data_interactor.py
+++ app/data_interactor.py
@@ -1,8 +1,9 @@
 import mysql.connector
 import os
 
+from models import Contact
 
-def get_db_connection():
+def get_db_connection()
     '''
     Docstring for get_db_connection
     dont frogat to do: " connection.close() "
@@ -19,3 +20,40 @@ def get_db_connection():
     except mysql.connector.Error as err:
         print("Error", err)
         return None
+
+
+
+def get_all_contacts() -> list[Contact] | None:
+    connection = get_db_connection()
+    if connection is None:
+        return []
+    
+    try:
+        cursor = connection.cursor()
+        query = "SELECT id, first_name, last_name, phone_number FROM contacts;"
+        cursor.execute(query)
+        results = cursor.fetchall()
+        print(results)
+        if results:
+            contacts = []
+            for row in results:
+                contact= Contact(
+                    id = row["id"],
+                    first_name = row["first_name"],
+                    last_name = row["last_name"],
+                    phone_number = row["phone_number"]
+                    )
+                contacts.append(contact)
+            cursor.close()
+            return contacts
+
+        else:
+            print("error-in:  --results = cursor.fetchall()--   is a Nune ")
+
+
+    except Exception as a:
+        print(f"error{a}")
+
+    finally:
+        connection.close()
+
